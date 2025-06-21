944e2a9e8ee0bf5f72a35e1d00766546811bc1a6
53rao <raox58@gmail.com>
2025-06-21 18:41:11 +0530

    Added the Template

---
+++ App_Template/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+   <>
+    <div className="bg-black min-h-screen w-screen flex item-center justify-center">
+          <button  className="px-1 py-3"><a href="https://github.com/53rao" className="text-4xl  text-white bg-emerald-500 p-4 rounded-md ">53Rao</a></button>
+
+    </div>
+    </>
+  );
+}
+
+export default App
